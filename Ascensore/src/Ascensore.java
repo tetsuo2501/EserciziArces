d384222c6e4f9d204516675dc5dfb7f51582d0a8
tetsuo2501 <tetsuo2501@gmail.com>
2015-04-24 20:08:26 +0200

    Primo Commit

---
+++ Ascensore/src/Ascensore.java
@@ -0,0 +1,13 @@
+/**
+ * Created by leonardo on 24/04/15.
+ */
+public class Ascensore {
+
+    private Prenotazione listaPrenotazioni[];
+
+
+    Ascensore(int maxPrenotazioni){
+        listaPrenotazioni = new Prenotazione[maxPrenotazioni];
+
+    }
+}
---
+++ Ascensore/src/Prenotazione.java
@@ -0,0 +1,23 @@
+/**
+ * Created by leonardo on 24/04/15.
+ */
+public class Prenotazione {
+    private int numeroPersone;
+    private int numeroPiano;
+
+    public int getNumeroPersone() {
+        return numeroPersone;
+    }
+
+    public void setNumeroPersone(int numeroPersone) {
+        this.numeroPersone = numeroPersone;
+    }
+
+    public int getNumeroPiano() {4
+        return numeroPiano;
+    }
+
+    public void setNumeroPiano(int numeroPiano) {
+        this.numeroPiano = numeroPiano;
+    }
+}
